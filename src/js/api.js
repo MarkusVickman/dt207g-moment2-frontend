2d766f713d214924be564d6929632f66a536c1c7
Markus Vickman~ <markus.vickman@hotmail.com>
2024-04-08 11:05:53 +0200

    styling med css och fixat ett enkelt get anrop

--- src/js/api.js
+++ src/js/api.js
@@ -1,14 +1,44 @@
 
-let url = "https://dt207g-moment2.onrender.com/api/cv/";
+let url = "localhost:3000/api/cv";
 
 
 
 /*funktionen som gör alla api-anrop med async await. Nykeln är en variabel i global scope. url och hostadresser skickas in i funktionen när den anropas.*/
-async function api() {
-      const response = await fetch(url)
+/*async function api(url) {
+      const response = await fetch(url);
 
       const result = await response.json();
         console.log(result);  
   }
 
-  api();
+  api(url);
+*/
+
+/*
+async function api(url) {
+      try {
+            const response = await fetch(url);
+            const result = await response.json();
+            console.log(result);
+
+      } catch (error) {
+            console.error(error);
+      }
+}
+
+api(url);
+*/
+api();
+
+async function api() {
+      let response = await fetch('http://localhost:3000/api/cv', {
+            method: 'GET',
+            headers: {
+                  'Content-Type': 'application/json'
+            },
+           // body: JSON.stringify(user)
+      });
+
+      let data = await response.json();
+      console.log(data);
+}
